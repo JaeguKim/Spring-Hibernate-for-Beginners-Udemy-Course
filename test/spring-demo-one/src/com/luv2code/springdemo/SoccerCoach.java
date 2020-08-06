db2c964513cf0e794301293c703025296d816f75
김재구 <rlaworn1993@gmail.com>
2020-08-06 23:11:36 +0900

    Practive Activity #1 Completed : Inversion of control with XML

---
+++ test/spring-demo-one/src/com/luv2code/springdemo/SoccerCoach.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public class SoccerCoach implements Coach {
+	@Override
+	public String getDailyWorkout() {
+		return "Do Shooting Practice 20 Times Per Day!";
+	}
+}
