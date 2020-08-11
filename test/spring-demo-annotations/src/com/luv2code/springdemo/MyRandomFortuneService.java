cf9f47d03e1407e3f45e9a5901544134cdee2655
김재구 <rlaworn1993@gmail.com>
2020-08-11 20:17:53 +0900

    Practice Activity #6 - Bean Scopes with Annotations

--- test/spring-demo-annotations/src/com/luv2code/springdemo/MyRandomFortuneService.java
+++ test/spring-demo-annotations/src/com/luv2code/springdemo/MyRandomFortuneService.java
@@ -1,21 +1,54 @@
 package com.luv2code.springdemo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 
-import org.springframework.beans.factory.annotation.Value;
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRandomFortuneService implements FortuneService {
 	
-	@Value("${listOfValues}")
-	String[] randomFortunes;
+	private ArrayList<String> randomFortunes;
+	private String fileName = "fortune-data.txt";
 	Random myRandom = new Random();
+
+	public MyRandomFortuneService() {
+		System.out.println(">> In MyRandomFortuneService constructor");
+	}
+	
+	@PostConstruct
+	public void readFile() {
+		System.out.println(">> readFile()");
+		File theFile = new File(fileName);
+		System.out.println("Reading fortunes from file: "+theFile);
+		System.out.println("File exists: "+theFile.exists());
+		
+		// initialize array list
+		randomFortunes = new ArrayList<String>();
+		
+		// read fortunes from file
+		try (BufferedReader br = new BufferedReader(new FileReader(theFile))){
+			String tempLine;
+			
+			while ((tempLine = br.readLine()) != null) {
+				randomFortunes.add(tempLine);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@Override
 	public String getFortune() {
-		int rndIdx = myRandom.nextInt(randomFortunes.length);
-		return randomFortunes[rndIdx];
+		int rndIdx = myRandom.nextInt(randomFortunes.size());
+		String tempFortune = randomFortunes.get(rndIdx);
+		return tempFortune;
 	}
 
 }
