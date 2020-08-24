8eadf1c25bdeba2d36986223d7618abe75027dc8
김재구 <rlaworn1993@gmail.com>
2020-08-24 13:21:04 +0900

    completed create customer

--- test/web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ test/web-customer-tracker/src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -24,8 +24,8 @@ public class CustomerDAOImpl implements CustomerDAO {
 		// get the current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 		
-		// create a query
-		Query<Customer> theQuery = currentSession.createQuery("from Customer",
+		// create a query ... sort by last name
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName",
 				Customer.class);
 		
 		// execute query and get result list
@@ -36,4 +36,30 @@ public class CustomerDAOImpl implements CustomerDAO {
 		
 	}
 
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+		
+		// get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// save the customer ... finally LOL
+		currentSession.save(theCustomer);
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
